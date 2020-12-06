cbe8d14c0655121e8c394b97c92b93f14e08b996
pmaks1985 <pmaks1985@gmail.com>
2020-12-06 22:10:09 +0300

    начал 3 задание

--- blocks/content.php
+++ blocks/content.php
@@ -3,13 +3,27 @@
         <?php
         $images = scandir("images");
         for ($i = 2; $i < count($images); $i++) { ?>
-            <a href="full_image.php?file=<?= $images[$i]; ?>">
-                <!--target="_blank" для ссылки не добавлял, много лишних вкладок-->
-                <img src="/images/<?= $images[$i]; ?>">
+            <a href="?file=<?= $images[$i]; ?>" data-toggle="modal" data-target="#myModal">
+                <img src="/images/<?= $images[$i]; ?>" id="<?= $i ?>">
             </a>
 
         <?php
         }
         ?>
+    </div>
+</div>
+
+<div class="modal" id="myModal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <img src="images/<?= $_GET['file'] ?>">
+            </div>
+        </div>
     </div>
 </div>
--- index.php
+++ index.php
@@ -30,30 +30,13 @@
         <div class="row">Footer</div>
     </div>
 
-    <!-- <button type="button" data-toggle="modal" data-target="#myModal">Launch modal</button>
-    <div class="modal" id="myModal" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <p>Modal body text goes here.</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                </div>
-            </div>
-        </div>
-    </div> -->
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
