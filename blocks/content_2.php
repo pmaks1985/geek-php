ce50b3696ca3fea079c0a59e2f4f58bc7bcfd133
pmaks1985 <pmaks1985@gmail.com>
2020-12-07 10:07:55 +0300

    начал 2 задание, в процессе

--- blocks/content_2.php
+++ blocks/content_2.php
@@ -1,10 +1,10 @@
 <div class="col-md-10">
     <div class="d-flex flex-wrap">
         <?php
-        $images = scandir("images");
+        $images = scandir("image/small/");
         for ($i = 2; $i < count($images); $i++) { ?>
-            <a href="full_images.php?file=<?= $images[$i]; ?>">
-                <img src="/images/<?= $images[$i]; ?>" id="<?= $i ?>">
+            <a href="full_images_2.php?file=<?= $images[$i]; ?>">
+                <img src="/image/small/<?= $images[$i]; ?>" id="<?= $i ?>">
             </a>
 
         <?php
