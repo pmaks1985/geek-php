9691f12b1cb22743733d12331b3f3b249501b37f
pmaks1985 <pmaks1985@gmail.com>
2020-12-08 10:05:36 +0300

    подключил fansybox,разобрался, доделал 3 задание

--- blocks/content_3.php
+++ blocks/content_3.php
@@ -1,22 +1,14 @@
+<?php
+$dir1 = "image/small";
+$dir2 = "image/big";
+$files1 = array_slice(scandir($dir1), 2);
+$files2 = array_slice(scandir($dir2), 2);
+?>
 <div class="col-md-10">
     <div class="d-flex flex-wrap">
         <?php
-        $images = scandir("images");
-        for ($i = 2; $i < count($images); $i++) { ?>
-            <a href="/images/?file=<?= $images[$i]; ?>" data-toggle="modal" data-target=".bd-example-modal-lg">
-                <img src="/images/<?= $images[$i]; ?>" id="<?= $i ?>">
-            </a>
-
-        <?php
-        }
-        ?>
-    </div>
-    <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <div class="modal-content">
-                <img src="/images/<?= $_GET['file']; ?>">
-            </div>
-        </div>
+        for ($i = 0; $i < count($files1); $i++) { ?>
+            <a class="photo" href="<?= $dir2 . "/" . $files2[$i] ?>" target="_blank"><img src="<?= $dir1 . "/" . $files1[$i] ?>" alt="" /></a>
+        <?php } ?>
     </div>
 </div>
--- index.php
+++ index.php
@@ -12,10 +12,16 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
+    <script type="text/javascript" src="scripts/jquery.js"></script>
+    <script type="text/javascript" src="scripts/fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
+    <script type="text/javascript" src="scripts/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+    <link rel="stylesheet" type="text/css" href="scripts/fancybox/jquery.fancybox-1.3.4.css" media="screen" />
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $("a.photo").fancybox();
+        });
+    </script>
     <title>Geek-php</title>
 </head>
 
@@ -24,19 +30,11 @@
         <?php include "blocks/header.php"; ?>
         <div class="row">
             <div class="col-md-1">Left menu</div>
-            <?php include "blocks/content_4.php"; ?>
+            <?php include "blocks/content_3.php"; ?>
             <div class="col-md-1">Right menu</div>
         </div>
         <div class="row">Footer</div>
     </div>
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
 </body>
 
 </html>
