daab416856254ca75891c04211505b266b332419
pmaks1985 <pmaks1985@gmail.com>
2020-12-09 11:29:53 +0300

    сделал 1 и 2 задание

--- index.php
+++ index.php
@@ -3,9 +3,49 @@ include "config.php";
 
 $sql = "select * from images";
 $res = mysqli_query($connect, $sql);
-
-while ($data = mysqli_fetch_assoc($res)) {
-    print_r($data);
-}
 ?>
+<!doctype html>
+<html lang="en">
 
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="scripts/fancybox/jquery.fancybox-1.3.4.css" media="screen" />
+    <script type="text/javascript" src="scripts/jquery.js"></script>
+    <script type="text/javascript" src="scripts/fancybox/jquery.mousewheel-3.0.4.js"></script>
+    <script type="text/javascript" src="scripts/fancybox/jquery.fancybox-1.3.4.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $(".photo").fancybox();
+        });
+    </script>
+    <title>Geek-php</title>
+</head>
+
+<body>
+<div class="container">
+    <div class="col-md-10">
+        <div class="d-flex flex-wrap">
+            <?php
+            while ($data = mysqli_fetch_assoc($res)) {?>
+                <a class="photo" href="<?=$data['path_big'].$data['name']?>" target="_blank"><img src="<?=$data['path_small'].$data['name']?>" alt=""></a>
+            <?
+                $offset = strpos($data['name'], '.');
+                echo $offset;
+            }
+            ?>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+</body>
+
+</html>
