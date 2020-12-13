ee554de86c9bc4178c8755dcb953caa6362947cf
pmaks1985 <pmaks1985@gmail.com>
2020-12-13 13:23:50 +0300

    добавил в обработку функцию strip_tags()

--- server_for_reviews.php
+++ server_for_reviews.php
@@ -1,7 +1,7 @@
 <?php
 include "config.php";
-$name = $_POST['authors-name'];
-$review_text = $_POST['review-text'];
+$name = strip_tags($_POST['authors-name']);
+$review_text = strip_tags($_POST['review-text']);
 
 $sql = "insert into reviews (name, text_review) values ('$name', '$review_text')";
 if(mysqli_query($connect,$sql)){
