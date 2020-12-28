28d5dc6470ec5c9a7455074159673eb03b9294db
pmaks1985 <pmaks1985@gmail.com>
2020-12-28 10:16:14 +0300

    закончил регистрацию пользователя
    небольшие правки

--- pages/reg.php
+++ pages/reg.php
@@ -4,16 +4,19 @@
 <div class="row">
     <form action="../../engine/server.php" method="post" enctype="multipart/form-data">
         <? if ($_GET['reg-user'] === 'ok'): ?>
-            <p class="h5 mt-4 text-success">Вы успешно зарегистрировались</p>
-        <? elseif ($_GET['reg-user'] === 'fields-not-entered'): ?>
-            <p class="h5 text-danger mt-4">Заполнены не все обязательные поля</p>
+            <p class="h5 mt-4 text-success">Доброе пожаловать <?=$_GET['fio'];?>,<br>
+                Вы успешно зарегистрировались, <br>
+                просьба <a href="/index.php?page=auth">авторизироваться</a> на сайте.</p>
         <? endif; ?>
+        <? if (isset($_GET['errors_reg'])): ?>
+        <p class='text-danger mt-4'><?= $_GET['errors_reg']; ?>
+            <? endif; ?>
         <div class="form-group">
-            <label for="fio">Ваше имя<sup> *</sup>:</label>
+            <label for="fio">Ваше имя:<sup> *</sup>:</label>
             <input type="text" class="form-control" id="fio" name="fio">
         </div>
         <div class="form-group">
-            <label for="phone">Ваш телефон<sup> *</sup>:</label>
+            <label for="phone">Ваш телефон:<sup> *</sup>:</label>
             <input type="number" class="form-control" id="phone" name="phone">
         </div>
         <div class="form-group">
@@ -21,16 +24,16 @@
             <textarea class="form-control" id="address" name="address"></textarea>
         </div>
         <div class="form-group">
-            <label for="user-login">Ваш логин<sup> *</sup>:</label>
+            <label for="user-login">Ваш логин:<sup> *</sup>:</label>
             <input type="text" class="form-control" id="user-login" name="user-login" required>
         </div>
         <div class="form-group">
-            <label for="user-pass">Ваш пароль<sup> *</sup>:</label>
+            <label for="user-pass">Ваш пароль:<sup> *</sup>:</label>
             <input type="password" class="form-control" id="user-pass" name="user-pass" required>
         </div>
         <div class="form-group">
             <small><sup> *</sup>Поля, обязательные для заполнения</small>
         </div>
-        <input type="submit" class="btn btn-primary" value="Зарегистрироваться">
+        <input type="submit" class="btn btn-primary" value="Зарегистрироваться" name="registration">
     </form>
 </div>
